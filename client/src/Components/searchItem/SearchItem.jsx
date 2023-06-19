0e0c81129952a585bf78d0c6a435fea637ba875f
nhatquang2001 <nhatquang1022001@gmail.com>
2023-06-20 06:56:55 +0700

    listItem

---
+++ client/src/Components/searchItem/SearchItem.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import './searchItem.css';
+
+
+const SearchItem = () => {
+    return (
+    <div className="searchItem">
+        <img src="" alt="" className="siImg" />
+        <div className="siDesc">
+            <h1 className="siTitle">toa nha AAA</h1>
+            <span className="siDistance">500m tu trung tam</span>
+            <span className="taxi0p">co taxi</span>
+            <span className="siSubtitle">
+                AAAA
+            </span>
+            <span className="siFeatures">
+                dien tich 21m2 ca phong ngu
+            </span>
+            <span className="siCancel0p">Huy mien phi</span>
+            <span className="siCancel0pSubtitle">
+                Ban co th huy phong
+            </span>
+
+        </div>
+        <div className="siDetails">
+            <div className="siRating">
+                <span className="Excellent">Excellent</span>
+                <button>8.9</button>
+            </div>
+            <div className="siDetailTexts">
+                <span className="siPrice">3.000.000đ</span>
+                <span className="siTaxi0p">Đã bao gồm phí Taxi</span>
+                <button className="siCheckButton">Xem thêm</button>
+                
+            </div>
+        </div>
+    </div>
+    );
+};
+
+export default SearchItem
